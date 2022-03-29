bd81f11980651bd3721076a135ef03f7eeacd543
ERIK ADAIR ALVARENGA MARQUES <03339990107@labinfo.pp.ifms.edu.br>
2022-03-29 13:54:47 -0300

    subindo codigo

---
+++ script.js
@@ -0,0 +1,47 @@
+var numSort = []
+var numEsco = []
+
+function sorteio(){
+    var cont = 0
+    numSort=[]
+
+    while (cont < 6){
+        let num = Math.random() * 60
+        num = Math.ceil(num)
+        if(!numSort.includes(num)){
+            numSort[cont] = num
+            console.log(numSort)
+            cont ++
+        }
+        
+    }
+
+    document.getElementById("sorteados").innerHTML = numSort
+    contAcertos()
+}
+
+function getValor(valor, pos){
+    numEsco[pos] = Number(valor)
+    if(valor <= 0 || valor > 60){
+        alert("Numero invalido, digite um entre q e 60")
+        document.getElementById(`num${pos+1}`).value = ""
+    }else if(numEsco.includes(valor)){
+        alert("Numero repitido, escolha um outro numero! ")
+        document.getElementById(`num${pos+1}`).value = ""
+    }else{
+        numEsco[pos] = valor
+        console.log(numEsco)
+    }
+    
+}
+
+function contAcertos(){
+    let contA = 0
+    numEsco.forEach(function(value, index){
+        if(numSort.includes(value)){
+            contA =contA +1
+        }
+    })
+    document.getElementById("acertos").innerHTML = contA
+}
+
